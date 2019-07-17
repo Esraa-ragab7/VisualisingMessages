8e1dff821e75fc0fe5ed55363ecc2db478f3a52c
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-07-17 11:16:34 +0200

    Initial commit

---
+++ VisualisingMessages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualisingMessages
+//
+//  Created by Abdelrahman Abu Sharkh on 7/17/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
