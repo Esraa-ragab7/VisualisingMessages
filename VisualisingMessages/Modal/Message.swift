b7d41db2677b6433e85db792537f0c29dfe2e937
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-07-18 17:25:32 +0200

    Add Messages Models.

---
+++ VisualisingMessages/Modal/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  VisualisingMessages
+//
+//  Created by Abdelrahman Abu Sharkh on 7/18/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import Foundation
