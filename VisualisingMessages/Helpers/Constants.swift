675afb00452c9c61e4947eba908772efffb9d8ae
Esraa Ragab <esraa.ragab@seamlabs.com>
2019-07-18 17:26:12 +0200

    Add Constants file

---
+++ VisualisingMessages/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  VisualisingMessages
+//
+//  Created by Abdelrahman Abu Sharkh on 7/18/19.
+//  Copyright © 2019 Esraa Ragab. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct APIKeys {
+        static let googleAPI = "AIzaSyAbZ239oOQx0IO3-0T5XemXJ3SkMrfk5lA"
+    }
+}
